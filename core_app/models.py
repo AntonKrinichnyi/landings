85c05a3d1755aabe2825e4683598faac6766cdd7
Anton Krinichny <@anton.krinichnyi>
2026-04-25 22:17:22 +0300

    add requirements.txt file

--- core_app/models.py
+++ core_app/models.py
@@ -5,7 +5,7 @@ from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.dialects.postgresql import UUID
 
-from landings_app.connection import Base
+from core_app.connection import Base
 
 
 class Affiliate(Base):
--- core_app/routers.py
+++ core_app/routers.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import APIRouter, Depends
+
+from core_app.connection import get_db
 
 router = APIRouter()
 
 @router.get("/leads")
-async def get_leads():
+async def get_leads(db: AsyncSession = Depends(get_db)):
     pass
