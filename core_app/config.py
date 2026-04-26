5be584c0e91222a3ba9d5b23e890ea3d96b19b8e
Anton Krinichny <@anton.krinichnyi>
2026-04-26 08:51:08 +0300

    add decode bearerJwt token logic and create file for services tests

--- core_app/config.py
+++ core_app/config.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     db_url: str = "postgresql+asyncpg://app:app@localhost:5432"
+    secret_token: str = "realy_secret_token"
+    jwt_algorithm: str = "HS256"
 
     class Config:
         env_file = ".env"
---
+++ core_app/services.py
@@ -0,0 +1,36 @@
+from uuid import UUID
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jose.exceptions import JWTError
+from jose import jwt
+
+from core_app.config import settings
+
+
+_token_auth_scheme = HTTPBearer()
+
+
+async def token_auth(
+    credentials: HTTPAuthorizationCredentials = Depends(_token_auth_scheme),
+) -> UUID:
+    try:
+        payload = jwt.decode(
+            credentials.credentials,
+            settings.secret_token,
+            algorithms=[settings.jwt_algorithm],
+        )
+
+        raw_id = payload.get("id")
+
+        if raw_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Missing 'id' claim in token",
+            )
+        return UUID(str(raw_id))
+
+    except (JWTError, ValueError) as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
+        ) from e
